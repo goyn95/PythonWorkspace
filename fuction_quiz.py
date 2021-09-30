98594c0d92f74b6838dbc36996422057ee881801
goyn95 <goyn9501@gmail.com>
2021-09-30 23:30:53 +0900

    Day3_python

--- fuction_quiz.py
+++ fuction_quiz.py
@@ -6,6 +6,16 @@ def std_weight(height, gender):
         female_avWeight = round(height * height * 0.0021, 2)
         print("키 {0}cm 여자의 표준 체중은 {1}kg입니다.".format(height, female_avWeight))
 
-gender = "남자"
-height = 175
+gender = ''
+height = 0
+while True:
+    if gender == "남자" or gender == "여자":
+        break
+    gender = input("성별을 입력하세요. : (남자, 여자)")
+    if gender != "남자" and gender != "여자":
+        print("잘못 입력하셨습니다. 다시 입력하세요.")
+        continue
+    height = height + int(input("키를 입력하세요. : "))
+    
+    
 std_weight(height, gender)

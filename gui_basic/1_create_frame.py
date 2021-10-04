8843948f9caeb3e4907b7eaca8193d2b006ef01d
goyn95 <goyn9501@gmail.com>
2021-10-04 17:22:53 +0900

    Day1_GUI_python(Button)

--- gui_basic/1_create_frame.py
+++ gui_basic/1_create_frame.py
@@ -1,10 +1,31 @@
 from tkinter import *
+from tkmacosx import Button
 
 root = Tk()
 root.title("goyn GUI")
-# root.geometry("640x480") # 가로 * 세로
-root.geometry("640x480+400+100") # 가로 * 세로 + x좌표 + y좌표
 
-root.resizable(False, False) # x(너비), y(높이) 값 변경 불가(차 크기 변경 불가)
+btn1 = Button(root, text="버튼1")
+btn1.pack()
+
+btn2 = Button(root, padx=5, pady=10,text="버튼2")
+btn2.pack()
+
+btn3 = Button(root, padx=10, pady=5,text="버튼3")
+btn3.pack()
+
+btn4 = Button(root, width=10, height=3, text="버튼4")
+btn4.pack()
+
+btn5 = Button(root, fg="red", bg="yellow", text="버튼5")
+btn5.pack()
+
+# photo = PhotoImage(file="gui_basic/img.jpeg")
+# btn6 = Button(root, image=photo)
+# btn6.pack()
+
+def btncmd():
+    print("버튼이 클릭되었어요")
+btn7 = Button(root, text="동작하는 버튼", command=btncmd)
+btn7.pack()
 
 root.mainloop()

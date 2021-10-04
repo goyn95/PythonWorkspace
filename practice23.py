ab5aeb561e2d59bcd8bee925c57febf9dba1a98a
goyn95 <goyn9501@gmail.com>
2021-10-04 16:39:19 +0900

    Day7_python

---
+++ practice23.py
@@ -0,0 +1,15 @@
+# input : 사용자 입력을 받는 함수
+# language = input("무슨 언엉를 좋아하세요?")
+
+# dir : 어떤 객체를 넘겨줬을 때 그 객체가 어떤 변수와 함수를 가지고 있는지 표시
+# print(dir())
+# import random # 외장 함수
+# print(dir())
+# # import pickle
+# # print(dir())
+# # print(dir(random))
+# lst = [1,2,3]
+# print(dir(lst))
+
+name = "Jim"
+print(dir(name))
